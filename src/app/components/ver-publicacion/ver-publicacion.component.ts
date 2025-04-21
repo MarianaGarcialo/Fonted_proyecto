618e1aade2573ebd060f1916594881c788716ade
MarianaGarcialo <mariana.garcialo@amigo.edu.co>
2025-04-21 18:59:36 -0500

    first commit

---
+++ src/app/components/ver-publicacion/ver-publicacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-publicacion',
+  imports: [],
+  templateUrl: './ver-publicacion.component.html',
+  styleUrl: './ver-publicacion.component.css'
+})
+export class VerPublicacionComponent {
+
+}
