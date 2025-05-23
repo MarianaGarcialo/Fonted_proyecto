5cb1c3b488749b49b6453bec3303b977c65ee27e
MarianaGarcialo <mariana.garcialo@amigo.edu.co>
2025-05-22 21:39:12 -0500

    frontend_finalizado

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const Environment = {
     production: false,
     baseUrl: 'https://constitcionfeminixta.vercel.app/',
-    backendUrl: 'https://backend-constitucon.onrender.com/'
+    backendUrl: 'https://backend-constitucon.onrender.com/api/'
   };
